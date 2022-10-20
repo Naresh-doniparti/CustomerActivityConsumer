0aa5043f5fff60ef09298202cd74cc788c898bf0
Naresh-Doniparti <naresh.doniparti@gmail.com>
2022-10-20 17:04:35 +0530

    initial commit with dependencies

---
+++ src/main/java/com/wf/wimt/CustomerActivityConsumer/CustomerActivityConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.wf.wimt.CustomerActivityConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerActivityConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerActivityConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wf/wimt/CustomerActivityConsumer/CustomerActivityConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wf.wimt.CustomerActivityConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerActivityConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
